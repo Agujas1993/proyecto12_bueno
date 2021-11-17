bcca57612a750dfa1dadc0b38d852de5ded97fad
Agujas1993 <samuel.smr2.local@gmail.com>
2021-11-17 18:04:36 +0100

    borrado suave, borrado fuerte, tests, papelera

---
+++ resources/views/errors/400.blade.php
@@ -0,0 +1,9 @@
+@extends('layout')
+
+@section('title', 'Página no encontrada')
+
+@section('content')
+
+    <h1>Acción no autorizada</h1>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -15,16 +15,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('usuarios', 'UserController@index')->name('users.index');
 Route::get('usuarios/crear', 'UserController@create')->name('users.create');
 Route::post('usuarios', 'UserController@store')->name('users.store');
+Route::get('usuarios/papelera', 'UserController@trashed')->name('users.trashed');
 Route::get('usuarios/{user}/editar', 'UserController@edit')->name('users.edit');
 Route::get('usuarios/{user}', 'UserController@show')
     ->where('id', '[0-9]+')->name('users.show');
 Route::put('usuarios/{user}', 'UserController@update')->name('users.update');
 Route::delete('usuarios/{user}', 'UserController@destroy')->name('users.destroy');
+Route::patch('usuarios/{user}/papelera', 'UserController@trash')->name('users.trash');
+Route::get('usuarios', 'UserController@index')->name('users.index');
 
 Route::get('editar-perfil', 'ProfileController@edit');
 Route::put('editar-perfil', 'ProfileController@update');
+
+Route::get('profesiones', 'ProfessionController@index')->name('professions.index');
+Route::delete('profesiones/{profession}', 'ProfessionController@destroy');
+
+Route::get('habilidades', 'SkillController@index')->name('skills.index');
 
 Route::get('saludo/{name}/{nickname?}', 'WelcomeUserController');
