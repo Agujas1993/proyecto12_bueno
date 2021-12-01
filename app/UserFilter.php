a700d7e5354e83f874310d8c98ffbd739a507080
Agujas1993 <samuel.smr2.local@gmail.com>
2021-12-01 18:05:29 +0100

    Filtrando por habilidades, hay un error

--- app/UserFilter.php
+++ app/UserFilter.php
@@ -2,33 +2,47 @@
 
 namespace App;
 
+use Illuminate\Support\Facades\DB;
+
 class UserFilter extends QueryFilter
 {
 
     public function rules(): array
     {
-        return[
+        return [
             'search' => 'filled',
             'state' => 'in:active,inactive',
-            'role' => 'in:user,admin'
+            'role' => 'in:user,admin',
+            'skills' => 'array|exists:skills,id'
         ];
     }
 
-    public function filterBySearch($query, $search) //se le quita "scope" ya que al estar en la clase que extiende de builder no hace falta
+    public function filterBySearch($query, $search)
     {
-        return $query->where(function ($query) use ($search){
-            $query->whereRaw('CONCAT(first_name, " ", last_name) like ?', "%{$search}%") //Para buscar por nombre completo
-            ->orWhere('email','LIKE' ,"%{$search}%")
-                ->orWhereHas('team', function ($query) use ($search){
-                    $query->where('name', 'LIKE',"%{$search}%");
-                    //Hacer test comprobar dos filtros simultáneos
+        return $query->where(function($query) use ($search) {
+            $query->whereRaw('CONCAT(first_name, " ", last_name) like ?', "%{$search}%")
+                ->orWhere('email', 'LIKE', "%{$search}%")
+                ->orWhereHas('team', function ($query) use ($search) {
+                    $query->where('name', 'LIKE', "%{$search}%");
                 });
         });
+
     }
 
-    public function filterByState($query, $state)//Hay que ponerle 'filterBy' antes ya que lo estamos definiendo en el foreach
+    public function filterByState($query, $state)
     {
         return $query->where('active', $state == 'active');
     }
 
+    public function filterBySkills($query, $skills)
+    {
+        $subquery = DB::table('skill_user AS s')
+            ->selectRaw('COUNT(s.id)')
+            ->whereColumn('s.user_id', 'users.id')
+            ->whereIn('skill_id', $skills);
+
+        $query->addBinding($subquery->getBindings());
+
+        $query->where(DB::raw("({$subquery->toSql()})"), count($skills));
+    }
 }
--- config/database.php
+++ config/database.php
@@ -50,7 +50,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,  //Para que deje hacer un having sin un groupby
             'engine' => null,
         ],
 
